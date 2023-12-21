580b43d61e258784348f0d796f81988f63f4b153
Brian Lister <126961361+FabulousPBB@users.noreply.github.com>
2023-12-21 09:15:24 -0600

    🧬 Implement game and refactor round

--- projects/rock-paper-scissors/assets/js/rps.js
+++ projects/rock-paper-scissors/assets/js/rps.js
@@ -1,34 +1,177 @@
 const CHOICES = ['Rock', 'Paper', 'Scissors'];
+const LOOSE = 0;
+const WIN = 1;
+const TIE = 2;
 
 function getComputerChoice() {
-	return CHOICES[Math.floor(Math.random() * CHOICES.length)];
+	return Math.floor(Math.random() * CHOICES.length);
 }
 
 function playRound(playerSelection, computerSelection) {
-	const playerSelectionConverted =
-		playerSelection.slice(0, 1).toUpperCase() +
-		playerSelection.slice(1).toLowerCase();
-
-	if (playerSelectionConverted === computerSelection) {
-		return 'You choose the same item, try again.';
+	if (playerSelection === computerSelection) {
+		return TIE;
 	}
 
-	if (playerSelectionConverted === 'Rock' && computerSelection === 'Scissors') {
-		return `You win! ${playerSelection} beats ${computerSelection}`;
-	}
-
-	if (playerSelectionConverted === 'Paper' && computerSelection === 'Rock') {
-		return `You win! ${playerSelection} beats ${computerSelection}`;
-	}
-
-	if (
-		playerSelectionConverted === 'Scissors' &&
-		computerSelection === 'Paper'
-	) {
-		return `You win! ${playerSelection} beats ${computerSelection}`;
-	}
-
-	return `You loose! ${computerSelection} beats ${playerSelection}`;
+	// See https://medium.com/@jp.mfichtl/rock-paper-scissors-lizard-spock-or-why-math-is-awesome-for-coding-405dabe30f4
+	return ((playerSelection - computerSelection + CHOICES.length) % 3) % 2;
 }
 
-console.log(playRound('Rock', getComputerChoice()));
+function game() {
+	let playerScore = 0;
+	let computerScore = 0;
+
+	let playerChoice = Number(
+		// eslint-disable-next-line no-alert
+		prompt('Choose item by number - 1: Rock, 2: Paper, 3: Scissors'),
+	);
+	let computerChoice = getComputerChoice();
+
+	switch (playRound(playerChoice, computerChoice)) {
+		case LOOSE: {
+			console.log(
+				`You Loose! ${CHOICES[computerChoice]} beats ${CHOICES[playerChoice]}`,
+			);
+			computerScore++;
+			break;
+		}
+
+		case WIN: {
+			console.log(
+				`You Win! ${CHOICES[playerChoice]} beats ${CHOICES[computerChoice]}`,
+			);
+			playerScore++;
+			break;
+		}
+
+		default: {
+			console.log('You choose the same items, its a tie.');
+			break;
+		}
+	}
+
+	playerChoice = Number(
+		// eslint-disable-next-line no-alert
+		prompt('Choose item by number - 1: Rock, 2: Paper, 3: Scissors'),
+	);
+	computerChoice = getComputerChoice();
+
+	switch (playRound(playerChoice, computerChoice)) {
+		case LOOSE: {
+			console.log(
+				`You Loose! ${CHOICES[computerChoice]} beats ${CHOICES[playerChoice]}`,
+			);
+			computerScore++;
+			break;
+		}
+
+		case WIN: {
+			console.log(
+				`You Win! ${CHOICES[playerChoice]} beats ${CHOICES[computerChoice]}`,
+			);
+			playerScore++;
+			break;
+		}
+
+		default: {
+			console.log('You choose the same items, its a tie.');
+			break;
+		}
+	}
+
+	playerChoice = Number(
+		// eslint-disable-next-line no-alert
+		prompt('Choose item by number - 1: Rock, 2: Paper, 3: Scissors'),
+	);
+	computerChoice = getComputerChoice();
+
+	switch (playRound(playerChoice, computerChoice)) {
+		case LOOSE: {
+			console.log(
+				`You Loose! ${CHOICES[computerChoice]} beats ${CHOICES[playerChoice]}`,
+			);
+			computerScore++;
+			break;
+		}
+
+		case WIN: {
+			console.log(
+				`You Win! ${CHOICES[playerChoice]} beats ${CHOICES[computerChoice]}`,
+			);
+			playerScore++;
+			break;
+		}
+
+		default: {
+			console.log('You choose the same items, its a tie.');
+			break;
+		}
+	}
+
+	playerChoice = Number(
+		// eslint-disable-next-line no-alert
+		prompt('Choose item by number - 1: Rock, 2: Paper, 3: Scissors'),
+	);
+	computerChoice = getComputerChoice();
+
+	switch (playRound(playerChoice, computerChoice)) {
+		case LOOSE: {
+			console.log(
+				`You Loose! ${CHOICES[computerChoice]} beats ${CHOICES[playerChoice]}`,
+			);
+			computerScore++;
+			break;
+		}
+
+		case WIN: {
+			console.log(
+				`You Win! ${CHOICES[playerChoice]} beats ${CHOICES[computerChoice]}`,
+			);
+			playerScore++;
+			break;
+		}
+
+		default: {
+			console.log('You choose the same items, its a tie.');
+			break;
+		}
+	}
+
+	playerChoice = Number(
+		// eslint-disable-next-line no-alert
+		prompt('Choose item by number - 1: Rock, 2: Paper, 3: Scissors'),
+	);
+	computerChoice = getComputerChoice();
+
+	switch (playRound(playerChoice, computerChoice)) {
+		case LOOSE: {
+			console.log(
+				`You Loose! ${CHOICES[computerChoice]} beats ${CHOICES[playerChoice]}`,
+			);
+			computerScore++;
+			break;
+		}
+
+		case WIN: {
+			console.log(
+				`You Win! ${CHOICES[playerChoice]} beats ${CHOICES[computerChoice]}`,
+			);
+			playerScore++;
+			break;
+		}
+
+		default: {
+			console.log('You choose the same items, its a tie.');
+			break;
+		}
+	}
+
+	if (playerScore > computerScore) {
+		console.log(`Congrats, you win.`);
+	} else if (playerScore < computerScore) {
+		console.log(`Oh no, you lost.`);
+	} else {
+		console.log('It is a tie game.');
+	}
+}
+
+game();
