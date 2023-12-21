5ea75d540d1c0ec10570301fb56f9545370e56ac
Brian Lister <126961361+FabulousPBB@users.noreply.github.com>
2023-12-20 19:31:44 -0600

    🧬 Implement playing a round

--- projects/rock-paper-scissors/assets/js/rps.js
+++ projects/rock-paper-scissors/assets/js/rps.js
@@ -4,4 +4,31 @@ function getComputerChoice() {
 	return CHOICES[Math.floor(Math.random() * CHOICES.length)];
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+	const playerSelectionConverted =
+		playerSelection.slice(0, 1).toUpperCase() +
+		playerSelection.slice(1).toLowerCase();
+
+	if (playerSelectionConverted === computerSelection) {
+		return 'You choose the same item, try again.';
+	}
+
+	if (playerSelectionConverted === 'Rock' && computerSelection === 'Scissors') {
+		return `You win! ${playerSelection} beats ${computerSelection}`;
+	}
+
+	if (playerSelectionConverted === 'Paper' && computerSelection === 'Rock') {
+		return `You win! ${playerSelection} beats ${computerSelection}`;
+	}
+
+	if (
+		playerSelectionConverted === 'Scissors' &&
+		computerSelection === 'Paper'
+	) {
+		return `You win! ${playerSelection} beats ${computerSelection}`;
+	}
+
+	return `You loose! ${computerSelection} beats ${playerSelection}`;
+}
+
+console.log(playRound('Rock', getComputerChoice()));
