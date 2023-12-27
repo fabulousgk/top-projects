a4cf28efef2129503ff24d4534ac12c069cd8dfd
Brian Lister <brian@lister.design>
2023-12-26 20:25:44 -0600

    🧬 Implement setting pane with grid size change

--- projects/mouse-a-sketch/assets/js/main.js
+++ projects/mouse-a-sketch/assets/js/main.js
@@ -1,6 +1,7 @@
 const sketchArea = document.querySelector('#sketch-area');
 
 function generateGrid(size) {
+	sketchArea.textContent = '';
 	for (let i = 0; i < size * size; i++) {
 		const cell = document.createElement('div');
 		cell.style.width = `${(1 / size) * 100}%`;
@@ -14,4 +15,17 @@ function draw(event) {
 	event.target.style.backgroundColor = '#444444';
 }
 
-generateGrid(16);
+document.querySelector('#settings-button')?.addEventListener('click', () => {
+	sketchArea?.classList.toggle('hidden');
+	document.querySelector('#settings-pane')?.classList.toggle('hidden');
+});
+
+document.querySelector('#grid-size')?.addEventListener('input', (event) => {
+	document.querySelector('#grid-size-label').textContent = event.target.value;
+});
+
+document.querySelector('#grid-size')?.addEventListener('change', (event) => {
+	generateGrid(event.target.value);
+});
+
+generateGrid(55);
