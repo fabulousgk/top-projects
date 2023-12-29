8d91656a1026d44f4fa84b805854667503bddda6
Brian Lister <brian@lister.design>
2023-12-29 14:29:55 -0600

    🧬 Implement digit input

--- projects/calculator/assets/js/main.js
+++ projects/calculator/assets/js/main.js
@@ -1,5 +1,8 @@
 /* global textFit */
 
+const calcStack = [];
+let entryMode = false;
+
 function resizeText() {
 	const calcLines = document.querySelectorAll('#screen div');
 	for (const line of calcLines) {
@@ -7,5 +10,86 @@ function resizeText() {
 	}
 }
 
+function updateDisplay() {
+	for (let i = 0; i < 8; i++) {
+		document.querySelector(`#line${i + 1}`).textContent = calcStack[i];
+	}
+
+	if (entryMode) {
+		document.querySelector('#line1-indicator').textContent = '1►';
+		document.querySelector('#line1-div .textFitted').style.justifyContent =
+			'start';
+	} else {
+		document.querySelector('#line1-indicator').textContent = '1:';
+		document.querySelector('#line1-div .textFitted')?.removeAttribute('style');
+	}
+
+	resizeText();
+}
+
+function input(value) {
+	if (entryMode) {
+		calcStack[0] += value;
+	} else {
+		calcStack.unshift(value);
+		entryMode = true;
+	}
+
+	updateDisplay();
+}
+
 addEventListener('load', resizeText);
 addEventListener('resize', resizeText);
+
+for (const button of document.querySelectorAll('button')) {
+	button.addEventListener('click', (event) => {
+		switch (event.currentTarget.value) {
+			case '+': {
+				break;
+			}
+
+			case '-': {
+				break;
+			}
+
+			case '*': {
+				break;
+			}
+
+			case '/': {
+				break;
+			}
+
+			case '^': {
+				break;
+			}
+
+			case '.': {
+				break;
+			}
+
+			case 'ac': {
+				break;
+			}
+
+			case 'del': {
+				break;
+			}
+
+			case 'enter': {
+				if (entryMode) {
+					entryMode = false;
+				}
+
+				updateDisplay();
+				break;
+			}
+
+			default: {
+				console.log(event.currentTarget.value);
+				input(event.currentTarget.value);
+				break;
+			}
+		}
+	});
+}
